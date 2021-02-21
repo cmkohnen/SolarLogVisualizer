b73879e098bff11be318490c5d69e8964814f552
ChaosMelone9 <christoph.kohnen1@gmail.com>
2021-02-21 13:07:51 +0100

    Bmp SolarLogAPI to 4.0.1 & New way of getting API into local Repository

--- addAPI.sh
+++ addAPI.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
- BINDIR=$(dirname "$(readlink -fn "$0")")
+ BinDir=$(dirname "$(readlink -fn "$0")")
  # Change this depending on the version you want to install to your local repository.
- Version=4.0.0
- # shellcheck disable=SC2164
- cd "$BINDIR"
+ Version=4.0.1
+ cd "$BinDir" || return
+ wget "https://github.com/ChaosMelone9/SolarLogAPI/releases/download/$Version/SolarLogAPI-$Version.jar"
  mvn install:install-file -Dfile=SolarLogAPI-$Version.jar -DgroupId=me.meloni -DartifactId=SolarLogAPI -Dversion=$Version -Dpackaging=jar
+ rm "SolarLogAPI-$Version.jar"
